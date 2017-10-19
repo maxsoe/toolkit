3854a3c08adcb285050bdc19bd807e578377f739
Max Soe <maxsoe@gmail.com>
2017-10-19 15:31:16 +0100

    Toggle grids code simplified

--- js/helper-grids.js
+++ js/helper-grids.js
@@ -3,11 +3,11 @@ $(document).ready(function () {
     var tag = e.target.tagName.toLowerCase();
     // 104 is the "h" key
     if ( e.which === 104 && tag != 'input' && tag != 'textarea') {
-      $(".helper-grid-horizontal").toggleClass("show");
+      $(".helper-grid-horizontal").toggle();
     }
     // 103 is the "g" key
     if ( e.which === 103 && tag != 'input' && tag != 'textarea') {
-      $(".helper-grid-vertical").toggleClass("show");
+      $(".helper-grid-vertical").toggle();
     }
   });
 });
